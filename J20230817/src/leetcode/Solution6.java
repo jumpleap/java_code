55c92174fa1e3e6654f163d829ba9282a0105083
lin-codePlus <972606225@qq.com>
2023-08-22 21:07:47 +0800

    leetcode练习

---
+++ J20230817/src/leetcode/Solution6.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+/**
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 例如，121 是回文，而 123 不是。
+ */
+class Solution6 {
+    public boolean isPalindrome1(int x) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(x);
+        sb.reverse();
+
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(x);
+        return sb.toString().equals(sb1.toString());
+    }
+
+    //法二：循环判断重新求x的值，若是原来的值相同，则返回true
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int ret = 0;
+        int tmp = x;
+        while (tmp != 0) {
+            int num = tmp % 10;
+            ret = ret * 10 + num;
+            tmp /= 10;
+        }
+
+        if (ret == x) {
+            return true;
+        }
+        return false;
+    }
+}
