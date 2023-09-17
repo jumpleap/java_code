8d374287e5bb3467589953e78bacd4f3497c9aae
lin-codePlus <972606225@qq.com>
2023-09-17 23:11:07 +0800

    leetcode刷题

---
+++ J20230914/src/leetcode/Solution1.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ *从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ */
+class Solution1 {
+    public int[] levelOrder(TreeNode root) {
+        //判空
+        if (root == null) {
+            return new int[0];
+        }
+        //使用队列存储树的结点
+        Queue<TreeNode> queue = new LinkedList<>();
+        //把根节点入队
+        queue.offer(root);
+        //使用顺序表保存结点的值
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //队列不为空
+        while (!queue.isEmpty()) {
+            //弹出队头元素
+            TreeNode tmp = queue.poll();
+            //把值添加到顺序表中
+            list.add(tmp.val);
+
+            //判断弹出的结点是否右左右结点，有则入队
+            if (tmp.left != null) {
+                queue.offer(tmp.left);
+            }
+            if (tmp.right != null) {
+                queue.offer(tmp.right);
+            }
+        }
+        //申请数组
+        int[] array = new int[list.size()];
+        //把顺序表中的值存在数组中，并返回
+        for (int i = 0; i < array.length; i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+    }
+}
