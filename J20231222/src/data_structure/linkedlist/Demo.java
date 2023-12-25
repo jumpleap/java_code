ab19bbb23641d36dbde32d7aaa34ba4b15298e8c
lin-codePlus <972606225@qq.com>
2023-12-25 22:46:08 +0800

    LinkedList的使用

---
+++ J20231222/src/data_structure/linkedlist/Demo.java
@@ -0,0 +1,21 @@
+package data_structure.linkedlist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+        // 构造一个空的LinkedList
+        List<String> list1 = new LinkedList<>();
+        // List<String> list2 = new java.util.ArrayList<>();
+        list1.add("JavaSE");
+        list1.add("JavaWeb");
+        list1.add("JavaEE");
+
+        for (String s : list1) {
+            System.out.println(s);
+        }
+        // 使用ArrayList构造LinkedList
+        List<String> list3 = new LinkedList<>(list1);
+    }
+}
