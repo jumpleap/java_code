85b110597529f073b1d981e7307b518215f4aa48
lin-codePlus <972606225@qq.com>
2024-01-20 19:16:15 +0800

    springboot项目

---
+++ J20231222/src/test/Demo5.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        String s1 = "abcd";
+        String s2 = "abc";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.compareTo(s2));
+    }
+}
