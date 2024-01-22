2c36cddb2065182b2fa0407f4660dd2bf505536b
lin-codePlus <972606225@qq.com>
2024-01-22 22:27:49 +0800

    springboot项目

--- springboot/src/main/java/com/example/springboot/demos/HelloController.java
+++ springboot/src/main/java/com/example/springboot/demos/HelloController.java
@@ -9,4 +9,9 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @RequestMapping("/sayHi")
+    public String hi() {
+        return "Hi, Spring!";
+    }
 }
