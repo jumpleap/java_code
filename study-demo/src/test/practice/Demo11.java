842e557e836b0ceda4d2f0de5df1af07d9f83e38
lin-codePlus <972606225@qq.com>
2024-04-08 22:40:12 +0800

    leetcode

--- study-demo/src/test/practice/Demo11.java
+++ study-demo/src/test/practice/Demo11.java
@@ -96,4 +96,59 @@ public class Demo11 {
         // 若q的长度和arr1数组长度相同, 则说明s为t的子串
         return p == arr1.length;
     }
+
+    /*
+        给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+        回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+        例如，121 是回文，而 123 不是。
+        链接: https://leetcode.cn/problems/palindrome-number/description/
+     */
+
+    /**
+     * 思路分析: 把数字转为字符串, 然后把字符串逆置后和原来的字符串进行比较, 判断是否相同
+     * 时间复杂度: O(N)
+     * 空间复杂度: O(N)
+     * @param x
+     * @return
+     */
+    public boolean isPalindrome1(int x) {
+        // 把数字转为字符串
+        String s = String.valueOf(x);
+        // 初始化字符串
+        StringBuilder str = new StringBuilder();
+        // 把数字添加到字符串中
+        str.append(x);
+        // 逆置
+        str.reverse();
+
+        // 把str转为字符串再和s进行比较
+        return str.toString().equals(s);
+    }
+
+    /**
+     * 思路分析: 模拟; 把x重新进行一次模拟, 判断模拟后的数是否和原来的数相同
+     * 时间复杂度: O(logN)
+     * 空间复杂度: O(1)
+     * @param x
+     * @return
+     */
+    public boolean isPalindrome(int x) {
+        // 负数全部为false
+        if (x < 0) return false;
+        // 小于10大于等于0的数全部为true
+        if (x < 10) return true;
+
+        // 其他数进行判断
+        int ans = 0;
+        // 保存原来的值
+        int oldVal = x;
+        while (x != 0) {
+            // 获取原来的位数
+            ans = ans * 10 + x % 10;
+            // 获取下一位数
+            x /= 10;
+        }
+        // 比较是否相等
+        return oldVal == ans;
+    }
 }
